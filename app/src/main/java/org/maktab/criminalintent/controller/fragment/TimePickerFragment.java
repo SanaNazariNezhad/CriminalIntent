551107fecda6e19c0f8666419babcb88f8c80dd7
SanaNazariNezhad <sana.nazari1995@gmail.com>
2020-09-07 02:04:56 +0430

    Implement TimePicker dialog using onCreateView instead of onCreateDialog

--- app/src/main/java/org/maktab/criminalintent/controller/fragment/TimePickerFragment.java
+++ app/src/main/java/org/maktab/criminalintent/controller/fragment/TimePickerFragment.java
@@ -1,18 +1,14 @@
 package org.maktab.criminalintent.controller.fragment;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,27 +18,25 @@ import android.widget.TimePicker;
 
 import org.maktab.criminalintent.R;
 
-import java.sql.Time;
 import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class TimePickerFragment extends DialogFragment {
 
     public static final String ARGS_CRIME_TIME = "crimeTime";
     public static final String EXTRA_USER_SELECTED_TIME = "com.example.criminalintent.userSelectedTime";
+    public static final String BUNDLE_KEY_CRIME_DATE = "bundle_key_CrimeDate";
     private Date mCrimeDate;
 
     private TimePicker mTimePicker;
-    /*private Button mButtonOk,mButtonCancel;*/
+    private Button mButtonOk, mButtonCancel;
     private Calendar mCalendar;
 
     public TimePickerFragment() {
         // Required empty public constructor
     }
+
     public static TimePickerFragment newInstance(Date crimeDate) {
         TimePickerFragment fragment = new TimePickerFragment();
         Bundle args = new Bundle();
@@ -54,11 +48,53 @@ public class TimePickerFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrimeDate = (Date) getArguments().getSerializable(ARGS_CRIME_TIME);
+        if (savedInstanceState != null) {
+            Calendar calendar = (Calendar) savedInstanceState.getSerializable(BUNDLE_KEY_CRIME_DATE);
+            mCrimeDate = calendar.getTime();
+        } else {
+            mCrimeDate = (Date) getArguments().getSerializable(ARGS_CRIME_TIME);
+        }
         mCalendar = Calendar.getInstance();
     }
 
-    @NonNull
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.custom_timer_picker_fragment, container, false);
+
+        findViews(view);
+        initViews();
+
+        listeners();
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        extractTimeFromTimePicker();
+        outState.putSerializable(BUNDLE_KEY_CRIME_DATE, mCalendar);
+    }
+
+    private void listeners() {
+        mButtonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                extractTimeFromTimePicker();
+                sendResult(mCalendar);
+                dismiss();
+            }
+        });
+        mButtonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
+    /*@NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         LayoutInflater inflater = LayoutInflater.from(getActivity());
@@ -83,12 +119,12 @@ public class TimePickerFragment extends DialogFragment {
 
         AlertDialog dialog = builder.create();
         return dialog;
-    }
+    }*/
 
     private void findViews(View view) {
         mTimePicker = view.findViewById(R.id.time_picker_crime);
-        /*mButtonOk = view.findViewById(R.id.btn_ok);
-        mButtonCancel = view.findViewById(R.id.btn_cancel);*/
+        mButtonOk = view.findViewById(R.id.btn_ok);
+        mButtonCancel = view.findViewById(R.id.btn_cancel);
     }
 
     private void initViews() {
@@ -114,9 +150,9 @@ public class TimePickerFragment extends DialogFragment {
         int minute = mTimePicker.getMinute();
         int second = now.getSecond();
 
-        mCalendar.set(Calendar.HOUR,hour);
-        mCalendar.set(Calendar.MINUTE,minute);
-        mCalendar.set(Calendar.SECOND,second);
+        mCalendar.set(Calendar.HOUR, hour);
+        mCalendar.set(Calendar.MINUTE, minute);
+        mCalendar.set(Calendar.SECOND, second);
     }
 
     private void sendResult(Calendar userSelectedDate) {
