8995c5a71ff2d0561f3bcf1030489a06e513358b
SanaNazariNezhad <sana.nazari1995@gmail.com>
2020-09-07 02:06:31 +0430

    revisit.

--- app/src/main/java/org/maktab/criminalintent/controller/fragment/TimePickerFragment.java
+++ app/src/main/java/org/maktab/criminalintent/controller/fragment/TimePickerFragment.java
@@ -8,7 +8,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -60,7 +59,7 @@ public class TimePickerFragment extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.custom_timer_picker_fragment, container, false);
+        View view = inflater.inflate(R.layout.costume_timer_picker_fragment, container, false);
 
         findViews(view);
         initViews();
