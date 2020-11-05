29408941e9a2d99f11dff6d795ffe8546feac6b0
SanaNazariNezhad <sana.nazari1995@gmail.com>
2020-11-05 23:21:21 +0330

    revisit.

--- app/src/main/java/org/maktab/criminalintent/repository/CrimeDBRepository.java
+++ app/src/main/java/org/maktab/criminalintent/repository/CrimeDBRepository.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 public class CrimeDBRepository implements IRepository {
 
     private static CrimeDBRepository sInstance;
-
+    
     private CrimeDatabaseDAO mCrimeDAO;
     private Context mContext;
 
